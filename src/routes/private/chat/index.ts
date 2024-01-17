7310098698c60a24127cb66cd94242f730b3f217
Akash Mondal <akash2003mondal@gmail.com>
2024-01-17 22:04:17 +0530

    Update MongoDB and Redis URLs, add messageSeen endpoint

--- src/routes/private/chat/index.ts
+++ src/routes/private/chat/index.ts
@@ -1,7 +1,14 @@
 import express from "express"
 import PrivateConversation from "../../../model/Private-Conversation"
 const PrivateChatMessageRoute = express.Router()
-
+interface PrivateMessageSeen {
+    messageIds: string[];
+    memberId: string;
+    receiverId: string;
+    conversationId: string;
+    createdAt?: string;
+    updatedAt?: string;
+}
 PrivateChatMessageRoute.post("/message/:id", async (req, res) => {
     try {
         const _id = req.params.id
@@ -21,4 +28,30 @@ PrivateChatMessageRoute.post("/message/:id", async (req, res) => {
     }
 })
 
+
+interface PrivateMessageSeen {
+    messageIds: string[];
+    memberId: string;
+    receiverId: string;
+    conversationId: string;
+    createdAt?: string;
+    updatedAt?: string;
+}
+PrivateChatMessageRoute.post("/messageSeen/:id", async (req, res) => {
+    try {
+        const _id = req.params.id
+        const { seen } = req.body as { seen: PrivateMessageSeen }
+        
+        const privateChatList = await PrivateConversation.updateMany({ _id }, {
+            $push: {
+                "messages.$[].seenBy": seen.memberId
+            },
+        })
+
+        res.status(200).json(privateChatList)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: error })
+    }
+})
 export default PrivateChatMessageRoute
